81ada78b68af98c3a37682182504c82f877a7176
Juan Buitrago <juanbuitrago2002@outlook.com>
2024-02-18 03:41:52 -0500

    login

--- web/components/login/login.tsx
+++ web/components/login/login.tsx
@@ -1,79 +1,79 @@
-import React from 'react';
+'use client';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { auth } from '../../firebase';
+import { Router } from 'next/router';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const singIn = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        localStorage.setItem('user', JSON.stringify(user));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="min-h-screen bg-green-50 flex flex-col justify-center items-center">
       <div className="max-w-md w-full">
         <h1 className="text-4xl font-bold mb-2">Hola</h1>
         <p className="text-lg mb-8">Vamos a fortalecer el campo</p>
-        <div className="bg-white p-8 rounded-lg shadow-lg">
-          <div className="mb-4">
-            <label className="block text-gray-700">Nombre</label>
-            <div className="mt-1 relative rounded-md shadow-sm">
-              <input
-                type="text"
-                name="name"
-                className="block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
-                placeholder="Eulises Montoya"
-              />
-              <div className="absolute inset-y-0 right-0 flex items-center">
-                <i className="fas fa-check-circle text-green-500"></i>
+        <form onSubmit={singIn}>
+          <div className="bg-white p-8 rounded-lg shadow-lg">
+            <div className="mb-4">
+              <label className="block text-gray-700">Nombre</label>
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <input
+                  type="text"
+                  name="name"
+                  className="block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                  placeholder="Eulises Montoya"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <div className="absolute inset-y-0 right-0 flex items-center">
+                  <i className="fas fa-check-circle text-green-500"></i>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700">Contraseña</label>
-            <input
-              type="password"
-              name="password"
-              className="block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
-              placeholder="••••••••"
-            />
-          </div>
-
-          <div className="flex items-center justify-between mb-6">
-            <div className="flex items-center">
+            <div className="mb-4">
+              <label className="block text-gray-700">Contraseña</label>
               <input
-                id="remember_me"
-                type="checkbox"
-                className="h-4 w-4 text-green-600 focus:ring-green-500 border-gray-300 rounded"
+                type="password"
+                name="password"
+                className="block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                placeholder="••••••••"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
-              <label
-                htmlFor="remember_me"
-                className="ml-2 block text-sm text-gray-900"
-              >
-                Remember me
-              </label>
             </div>
-            <div className="text-sm">
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+              >
+                INICIA SESIÓN
+              </button>
+            </div>
+
+            <div className="mt-6 text-center">
               <a
                 href="#"
                 className="font-medium text-green-600 hover:text-green-500"
               >
-                Forgot Password?
+                ¿No tienes cuenta? Regístrate
               </a>
             </div>
           </div>
-
-          <div>
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-            >
-              INICIA SESIÓN
-            </button>
-          </div>
-
-          <div className="mt-6 text-center">
-            <a
-              href="#"
-              className="font-medium text-green-600 hover:text-green-500"
-            >
-              ¿No tienes cuenta? Regístrate
-            </a>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
---
+++ web/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAhRnfqffZQHzdInGExpVS8mlbdRbMA0ao',
+  authDomain: 'ecoecho-ba1f6.firebaseapp.com',
+  projectId: 'ecoecho-ba1f6',
+  storageBucket: 'ecoecho-ba1f6.appspot.com',
+  messagingSenderId: '278716934015',
+  appId: '1:278716934015:web:e29dfd25d9cbe7ef099c4a',
+  measurementId: 'G-BZFZZPZXJ4',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+const analytics = getAnalytics(app);
