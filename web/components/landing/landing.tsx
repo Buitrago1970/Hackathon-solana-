d83189c552d5f04d8e466077204c6bbf9adf426d
Juan Buitrago <juanbuitrago2002@outlook.com>
2024-02-17 22:23:25 -0500

    landing page

--- web/components/landing/landing.tsx
+++ web/components/landing/landing.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export function Landing() {
   return (
-    <div className="m-8 flex w-full">
-      <div className="w-1/2 mt-32">
-        <div className="flex font-semibold">
-          <h1 className="text-9xl text-green-600">Eco</h1>
-          <h1 className="text-9xl text-gray-800">Echo</h1>
+    <div className="mt-16 m-8 flex w-full">
+      <div className="w-[48%] mt-20">
+        <div className="flex font-extrabold">
+          <h1 className="text-[130px] text-green-600">ECO</h1>
+          <h1 className="text-[130px] text-gray-800">ECHO</h1>
           <h1></h1>
         </div>
         <p className="mb-5">
           ¡Siembra el Futuro! Únete a nuestra revolución verde.
         </p>
-        <div className="my-10">
+        <div className="mb-10 mt-5 w-1/2">
           <p>
             Transforma tu pasión por la naturaleza en acción con ReforestApp.
             Planta árboles, forja un futuro más verde y únete a una comunidad
@@ -25,32 +25,32 @@ export function Landing() {
           </button>
         </div>
       </div>
-      <div className="w-1/2 flex">
-        <div className="flex flex-col justify-end">
-          <div className="flex justify-center flex-col items-center">
-            <div>
+      <div className="w-[48%] flex">
+        <div className="flex flex-col justify-end ">
+          <div className="flex justify-center flex-col items-center mb-3">
+            <div className="mb-2">
               <img src="/img/fa_handshake-o.svg" alt="" />
             </div>
-            <h1>Únete a la Misión Verde</h1>
+            <h1>reducción de CO2</h1>
           </div>
           <div>
             <img src="/img/Rectangle 57.png" alt="alttt" />
           </div>
         </div>
         <div className="flex flex-col justify-end">
-          <div className="flex justify-center flex-col items-center">
-            <div>
+          <div className="flex justify-center flex-col items-center mb-3">
+            <div className="mb-2">
               <img src="/img/jam_search.svg" alt="" />
             </div>
-            <h1>Únete a la Misión Verde</h1>
+            <h1>lucha contra el cambio climatic</h1>
           </div>
           <div>
             <img src="/img/Rectangle 58.png" alt="alttt" />
           </div>
         </div>
         <div className="flex flex-col justify-end">
-          <div className="flex justify-center flex-col items-center">
-            <div>
+          <div className="flex justify-center flex-col items-center mb-3">
+            <div className="mb-2">
               <img src="/img/Subtract.svg" alt="" />
             </div>
             <h1>Únete a la Misión Verde</h1>
